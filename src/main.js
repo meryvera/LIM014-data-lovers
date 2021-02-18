1bd500542ee211959583f493602a31a24f5a5dbc
Kelly <kelmita22@gmail.com>
2021-02-18 18:27:07 -0500

    Javascript HU 1-6 dom

--- src/main.js
+++ src/main.js
@@ -1,56 +1,130 @@
-//import { example } from './data.js';
 import data from './data/lol/lol.js';
-import { filterRols } from './data.js';
-//import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filterChampion,filterRoles,filterLevels,sortChampionsDes} from './data.js';
 
-//HABILITANDO EL DROPDOWN DE BOTON ROLES Y NIVELES
-const roles = document.querySelector('.roles');
-const rolesIcon = document.querySelector('#rolesIcon');
-rolesIcon.onclick = function (){
-  roles.classList.toggle('is-active')
-}
-const levels = document.querySelector('.levels');
-const levelsIcon = document.querySelector('#levelsIcon');
-levelsIcon.onclick = function (){
-  levels.classList.toggle('is-active')
-}
-
-
-//querySelector del section en html para cargar los card
+// VARIABLES GLOBALES - CONVERSION DE LA DATA JSON DE OBJETOS A ARRAYS
 const cardcontainer = document.querySelector('.cardContainer');
-//Con Object.entries devuelvo el array a usar con su par key-value
-const champions = Object.entries(data.data);
-console.log(champions);
-//usamos map para recorrer el array y a la vez almacenarlos valores de la función para reusar de ser el caso.
-champions.map (element => {
-//imprimimos elementos contenedores de las card en pantalla.
-cardcontainer.innerHTML+=
- `<section class="card-container">
-    <div class="cardChampions">
-      <article class="card front">
-        <img class="img-fluid" alt= "${element[1].name}" src="${element[1].splash}" id="img">
-        <h4 class="card-title">${element[1].name}
-        <a style="float: right;" href="#"><img class="seemore-icon" src="img/flecha.png"></a></h4>
-      </article>
-      <article class="card back">
-        <h2 id="fontCard">${element[1].id}</h2>
-        <p id="fontCard">${element[1].blurb}</p>
-        <ul id="fontCard">Habilidades:
-          <li><a id="fontCard">Ataque: ${element[1].info.attack}<a/></li>
-          <li><a id="fontCard">Defensa: ${element[1].info.defense}<a/></li>
-          <li><a id="fontCard">Magia: ${element[1].info.magic}<a/></li>
-          <li><a id="fontCard">Dificultad: ${element[1].info.difficulty}<a/></li>
-        </ul>
-        </article>
-   </div>
- </section>`
+const AllChampions = Object.entries(data.data);
 
-  //  console.log(cardcontainer);
-})
+// FUNCION PARA CARGAR TODOS LOS CAMPEONES
+const cargarpagina = document.querySelector('body');
+cargarpagina.addEventListener('load', cardChampions(AllChampions))
+
+// FUNCIÓN DE LITERAL STRINGS PARA JALAR LOS 134 CAMPEONES
+function cardChampions(data) {
+  data.map (element => {
+    cardcontainer.innerHTML+=
+    `<section class="card-container">
+        <div class="cardChampions">
+          <article class="card front">
+            <img class="img-fluid" alt= "${element[1].name}" src="${element[1].splash}" id="img">
+            <h4 class="card-title">${element[1].name}
+            <a class="circulo" href="#"><img class="seemore-icon" src="img/flecha.png"></a></h4>
+          </article>
+          <article class="card back">
+            <h2 id="fontCard">${element[1].id}</h2>
+            <p class="fonth2" id="fontCard">${element[1].blurb}</p><hr>
+            <p><strong>Habilidades:<strong></p>
+            <ul class="lista" id="fontCard">
+              <li><a id="fontCard">Ataque: ${element[1].info.attack}<a/></li>
+              <li><a id="fontCard">Defensa: ${element[1].info.defense}<a/></li>
+              <li><a id="fontCard">Magia: ${element[1].info.magic}<a/></li>
+              <li><a id="fontCard">Dificultad: ${element[1].info.difficulty}<a/></li>
+            </ul><hr>
+            <p>Roles:</p>
+            <ul class="lista2">
+              <li><a id="fontCard">${element[1].tags[0]}<a><li>
+              <li><a id="fontCard">${element[1].tags[1]}<a><li>
+            </ul>
+          </article>
+      </div>
+    </section>`
+  });
+}
+
+//I. FUNCION PARA BUSQUEDA DE CAMPEONES
+//Parte 1 - Método para enlistar campeones en Datalist
+let datalist = document.getElementById("championsLol");
+let AllKeyChampions = Object.keys(data.data); 
+
+//Parte 2 - Funcion para enlistar opciones de campeones
+function createdOptionsDataList(){
+  for (let i = 0; i < AllKeyChampions.length; i++) {
+    let option = document.createElement("option");
+    let txt = document.createTextNode(AllKeyChampions[i]);
+    option.appendChild(txt);
+    datalist.insertBefore(option,datalist.lastChild);
+  }
+}
+//Parte 3 - Función para cuando se da click en el input se despliegue el datalist
+let inputSearch = document.getElementById('search');
+inputSearch.addEventListener('click', createdOptionsDataList());
+
+//Parte 4 - Función para seleccionar campeón
+inputSearch.addEventListener("change" , function() {
+  const selectSearch = inputSearch.value;
+
+  //4.1 - Filtrado y Sort de los AllChampions 
+  let filteredChampionLol = filterChampion(AllChampions, selectSearch);
+  const AllChampionsSort = sortChampionsDes(AllChampions);
+
+  cardcontainer.innerHTML = '';
+  cardChampions(filteredChampionLol);
+  if (selectSearch === "Orden Descendente"){
+    cardcontainer.innerHTML = ''; 
+    cardChampions(AllChampionsSort); 
+  }else if(selectSearch === "Todos los Campeones"){ 
+   cardcontainer.innerHTML = '';   
+   cardChampions(AllChampions); 
+  } 
+}); 
+  
+//II. FUNCIÓN DE FILTRADO Y DESFILTRADO DE ROLES */
+const selectRoles= document.getElementById('rolesLol');
+selectRoles.addEventListener('change', function functSelecRols() { 
+  let selectedOption = selectRoles.options[selectRoles.selectedIndex].value; 
+  let filteredRolesLol = filterRoles(AllChampions, selectedOption); 
+
+  cardcontainer.innerHTML = ''; 
+  cardChampions(filteredRolesLol); 
+  if(selectedOption == "BackAllRoles"){ 
+    cardcontainer.innerHTML = ''; 
+    cardChampions(AllChampions); 
+  }
+}); 
+
+//III. FUNCION PARA BUSQUEDA DE NIVELES 
+const selectLevels= document.getElementById('levelsLol'); 
+selectLevels.addEventListener('change', function functSelecLevels() { 
+  let selectedLevelOption = selectLevels.options[selectLevels.selectedIndex].value; 
+  let filteredLevelsLol = filterLevels(AllChampions, selectedLevelOption); 
+
+  if (selectedLevelOption == "Bajo"){ 
+    cardcontainer.innerHTML = ''; 
+    cardChampions(filteredLevelsLol); 
+  } else if (selectedLevelOption == "Moderado"){ 
+    cardcontainer.innerHTML = ''; 
+    cardChampions(filteredLevelsLol); 
+  }else if (selectedLevelOption == "Alto"){ 
+    cardcontainer.innerHTML = ''; 
+    cardChampions(filteredLevelsLol); 
+  } else { 
+    cardcontainer.innerHTML = ''; 
+    cardChampions(AllChampions); 
+  } 
+}) 
 
 
-console.log(example, data);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
