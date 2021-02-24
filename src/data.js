13dc2d0755a90952c898f6c9e06ff792c2226fe0
kelmita22 <kelmita22@gmail.com>
2021-02-24 18:08:12 -0500

    Cierre de función estadística

--- src/data.js
+++ src/data.js
@@ -4,36 +4,61 @@ export const filterChampion = (data, selected) =>{
   return filteredChampion;
 }
 
-// FUNCION PARA ORDEN DESCENDENTE 
-export const sortChampionsDes = (data) =>{
-  const AllChampionsSort = data.sort((prev, next) => {
-    if(prev > next) return -1;
-     return 0;
-  })
-  return AllChampionsSort;
-}
+// FUNCION PARA ORDEN ZA
+export const sortChampionsDes = (data, selected) =>{
+  switch (selected){
+    case "za":
+      return data.reverse()
+         }
+  }; 
+
+// FUNCION PARA ORDEN AZ
+export const sortChampionsAsc = (data, selected) =>{
+  switch (selected){
+    case "az":
+      return data.sort()
+         }
+  }; 
+
 
 // FUNCION MODULO DE FILTRADO DE ROLES
-export const filterRoles = (data, selected) => { 
-let filteredRoles = data.filter(data => data[1].tags[0] == selected || data[1].tags[1] == selected); 
-return filteredRoles; 
-}; 
+export const filterRoles = (data, selected) => {
+let filteredRoles = data.filter(data => data[1].tags[0] == selected || data[1].tags[1] == selected);
+return filteredRoles;
+};
 
-// FUNCION MODULO DE FILTRADO DE NIVELES 
+// FUNCION MODULO DE FILTRADO DE NIVELES
 export const filterLevels = (data, selected) => { 
-let filteredLevelLolbajo = data.filter(data => data[1].info.difficulty <= 3); 
-let filteredLevelLolmoderado = data.filter(data => data[1].info.difficulty >= 4 && data[1].info.difficulty <= 6); 
-let filteredLevelLolalto = data.filter(data => data[1].info.difficulty >= 7); 
+  switch (selected){
+    case "Bajo":
+      return data.filter(data => data[1].info.difficulty <= 3);
+    case "Moderado":
+      return data.filter(data => data[1].info.difficulty >= 4 && data[1].info.difficulty <= 6);
+    case "Alto":
+      return data.filter(data => data[1].info.difficulty >= 7);
+  }
+  };
 
-if (selected == "Bajo"){ 
-return (filteredLevelLolbajo);
-} else if (selected == "Moderado"){ 
-return (filteredLevelLolmoderado);
-}else if (selected == "Alto"){ 
-return (filteredLevelLolalto);
+// FUNCION MODULO DE CÁLCULO DE DAÑO DE ATAQUE
+export const attack = (ataque, armadura) => {
+  let attackCalculate = (ataque*100) / (100 + armadura);
+  return attackCalculate ;
 }
+
+// FUNCION MODULO DE CÁLCULO DE DAÑO DE ATAQUE BÁSICO ESTADÍSTICA
+export const attackStats = (ataque, armadura) => {
+  let newArraycalculate=[]
+  for(let i=0; i < ataque.length; i++){
+    let attackCalculate = (ataque[i]*100) / (100 + armadura[i]);
+    newArraycalculate.push(attackCalculate)
+  }
+  return newArraycalculate;
 }
-  
-  
-  
+
+
+
+
+
+
+
 
