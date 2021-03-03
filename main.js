a145032ba403f97d36f27af3fc5a0a76ce969dff
meryvera <yrem.vera.5@gmail.com>
2021-03-03 13:57:41 -0500

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import data from './data/lol/lol.js';
-import { filterChampion,filterRoles,filterLevels,sortChampionsDes, sortChampionsAsc,attack, attackStats} from './data.js';
+import { filterChampion,filterRoles,filterLevels,sortChampionsDes, sortChampionsAsc,attack, /* attackStats */} from './data.js';
 
 // VARIABLES GLOBALES - CONVERSION DE LA DATA JSON DE OBJETOS A ARRAYS
 const cardcontainer = document.querySelector('.cardContainer');
@@ -19,18 +19,18 @@ function cardChampions(data) {
           <article class="card front">
             <img class="img-fluid" alt= "${element[1].name}" src="${element[1].splash}" id="img">
             <h4 class="card-title">${element[1].name}
-            <a class="circulo" href="#"><img class="seemore-icon" src="img/flecha.png"></a></h4>
+            <a class="circulo"><img class="seemore-icon" src="img/flecha.png"></a></h4>
           </article>
           <article class="card back">
             <h2 id="fontCard">${element[1].id}</h2>
-            <p class="fonth2" id="fontCard">${element[1].blurb}</p><hr>
+            <p class="fonth2" id="fontCard">${element[1].blurb}</p><hr></br>
             <p><strong>Habilidades:<strong></p>
             <ul class="lista" id="fontCard">
               <li><a id="fontCard">Ataque: ${element[1].info.attack}<a/></li>
               <li><a id="fontCard">Defensa: ${element[1].info.defense}<a/></li>
               <li><a id="fontCard">Magia: ${element[1].info.magic}<a/></li>
               <li><a id="fontCard">Dificultad: ${element[1].info.difficulty}<a/></li>
-            </ul><hr>
+            </ul><hr></br>
             <p>Roles:</p>
             <ul class="lista2">
               <li><a id="fontCard">${element[1].tags[0]}<a><li>
@@ -56,7 +56,7 @@ function cardChampions(data) {
       datalist.insertBefore(option,datalist.lastChild);
     }
   }
-  
+
   //Parte 3 - Función para cuando se da click en el input se despliegue el datalist
   let inputSearch = document.getElementById('search');
   inputSearch.addEventListener('click', createdOptionsDataList());
@@ -122,51 +122,46 @@ a_semiExperts.addEventListener('click', functSemiExperts)
 
 function functSemiExperts () {//creo qe event prevent default va a aca cuando arriba la funcion no tiene ()
   wrapper.innerHTML = `
-    <section>
-      <h1>Cálculo Daño de Ataque</h1>
+    <div class="containerData">
+      <h2 class="h2-conocedores">Cálculo de daño de ataque</h2>
       <section class="datos">
         <img src="img/input.png" class="champion">
-          <section class="containerForm">
-            <form name="formulario" class=>
-              <label class="labels">Ingresa el daño de ataque o poder de habilidad</label>
-              <input class="input" type="number" name="ataque" id="ataqueHabilidad" placeholder="Ataque o Habilidad"><br>
-              <label class="labels">Ingresa armadura o Resistencia mágica</label>
-              <input class="input" type="number" name="armadura" id="armaduraResistencia" placeholder="Armadura o resistencia mágica"><br>
-              <button class="submit" id="boton-calcular">Calcular</button>
-              <input class="input" id="resultado">
-            </form>
-          </section>
+        <form name="formulario" class="containerForm">
+          <label class="labels">Ingresa el daño de ataque o poder de habilidad</label>
+          <input class="input" type="number" name="ataque" id="ataqueHabilidad" placeholder="Ataque o Habilidad"><br>
+          <label class="labels">Ingresa armadura o Resistencia mágica</label>
+          <input class="input" type="number" name="armadura" id="armaduraResistencia" placeholder="Armadura o resistencia mágica"><br>
+          <button class="submit" id="boton-calcular">Calcular</button>
+          <input class="input" id="resultado" placeholder="Resultado">
+        </form>
       </section>
-    </section>
-    <section>
-      <canvas class="canvas" id="myChart" width="900" height="700"></canvas>
-    </section>
+    </div>
   `
+  /* <section> */
+  /* <canvas class="canvas" id="myChart" width="900" height="700"></canvas> */
+  /* </section> */
+
   //HACK CONOCEDORES - FUNCION PARA HACER CÁLCULO DE CALCULO BÁSICO DE 1 ATAQUE
   const botonCalcular = document.getElementById('boton-calcular');
   botonCalcular.addEventListener('click', function(event) {
     event.preventDefault();
     const ataque = parseFloat(document.getElementById('ataqueHabilidad').value);
-    /* console.log(ataque); */
     const armadura = parseFloat(document.getElementById('armaduraResistencia').value);
-   /*  console.log(armadura); */
     const resultado = document.getElementById('resultado');
-   /*  console.log(resultado); */
 
     let calcular = attack(ataque, armadura)
-      /* console.log(typeof attack); */
 
       resultado.value = calcular;
   })
 
   // HACK CONOCEDORES -CÁLCULO DE CALCULO BÁSICO DE 1 ATAQUE
-   const valoresArmor= AllChampions.map(item => item[1].stats.armor) 
-   /* console.log(valoresArmor)  */
-   const valoresAtaque = AllChampions.map(item2 =>item2[1].stats.attackdamage) 
-   let calculateDamage = attackStats(valoresAtaque, valoresArmor); 
- /*   console.log(calculateDamage)  */
-   
-   const ctx = document.querySelector('#myChart').getContext('2d');
+  //  const valoresArmor= AllChampions.map(item => item[1].stats.armor)
+  //  /* console.log(valoresArmor)  */
+  //  const valoresAtaque = AllChampions.map(item2 =>item2[1].stats.attackdamage)
+  //  let calculateDamage = attackStats(valoresAtaque, valoresArmor);
+  //   console.log(calculateDamage)
+
+/*    const ctx = document.querySelector('#myChart').getContext('2d');
    new Chart(ctx,{
 
        type :'line',
@@ -242,4 +237,15 @@ function functSemiExperts () {//creo qe event prevent default va a aca cuando ar
            }
          }
      })
+   Chart(); */
+}
+
+//FUNCIÓN DE MENÚ HAMBURGUESA
+    document.querySelectorAll(".more")[0].addEventListener("click",menu);
+
+    function menu(event){
+      event.preventDefault();
+      const allElements = document.querySelector('.menu');
+      allElements.classList.toggle('is-active');
+
     }
